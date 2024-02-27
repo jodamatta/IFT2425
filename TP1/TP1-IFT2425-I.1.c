12bd1a4a5ab3298baae29bcb5787f97c3ca31a85
Joana da Matta <108022074+jodamatta@users.noreply.github.com>
2024-02-27 09:23:37 -0500

    add Q1 to teacher's code

--- TP1/TP1-IFT2425-I.1.c
+++ TP1/TP1-IFT2425-I.1.c
@@ -1,7 +1,7 @@
 //------------------------------------------------------
 // module  : Tp-IFT2425-I.1.c
-// author  : 
-// date    : 
+// author  : Joana da Matta Furtado Ferreira
+// date    : 29-01-2024
 // version : 1.0
 // language: C
 // note    :
@@ -307,6 +307,38 @@ void Egalise(float** img,int lgth,int wdth,int thresh)
    img[i][j]=FnctRept[(int)(img[i][j])];
 }
 
+double f(double y[], int N, double c) {
+    double numerator = 0.0;
+    double denominator = 0.0;
+    double sum_log_y = 0.0;
+
+    for (int i = 0; i < N; ++i) {
+        numerator += pow(y[i], c) * log(y[i]);
+        denominator += pow(y[i], c);
+        sum_log_y += log(y[i]);
+    }
+    return numerator / denominator - 1.0 / c - sum_log_y / N;
+}
+
+double derivativeA(double y[], int N, double c, double epsilon) {
+    return (f(y, N, c + epsilon) - f(y, N, c)) / epsilon;
+}
+
+double methode_newtonA(double y[], int N, double initial_guess, double tol) {
+    double c_n = initial_guess;
+
+    for (int i = 0; i < 100; ++i) {
+        double f_c_n = f(y, N, c_n);
+        double f_prime_c_n = derivativeA(y, N, c_n, EPSILON);
+        c_n = c_n - f_c_n / f_prime_c_n;
+
+        if (fabs(f_c_n) < tol) {
+            break;
+        }
+    }
+    return c_n;
+}
+
 
 //----------------------------------------------------------
 //----------------------------------------------------------
@@ -348,6 +380,15 @@ int main(int argc,char** argv)
    //---------------------------
 
    //implementer ici
+
+     int N = 10;
+     double y_values[] = { 0.11,0.24,0.27,0.52,1.13,1.54,1.71,1.84,1.92,2.01 };
+
+     double initial_guess = 0.25;
+     double tol = 1e-6;
+
+     double root = methode_newtonA(y_values, N, initial_guess, tol);
+     printf("Q1A: racine trouve: %lf\n", root);
  
 
   
